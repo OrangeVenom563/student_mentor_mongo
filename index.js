17a5edab0f908ac1c4ee540b5625329eacd090f0
Lawrence J <lawrenceg563@gmail.com>
2021-09-21 12:30:45 +0530

    created util method to connect to mongo db server starts listerning after connecting to mongodb

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
+const mongoConnect = require('./utils/database').mongoConnect;
 
 app.use(express.json())
 app.use(cors())
 app.get('/',(req,res)=>{
-    res.send('Hello from express')
+    res.send('Hello welcome to student mentor portal')
 })
 
 app.use('/student',require('./routes/student'))
@@ -14,4 +15,6 @@ app.use('/mentor',require('./routes/mentor'))
 
 app.use(require('./controller/error'));
 
-app.listen(process.env.PORT||5000,()=>console.log('server running'))
+mongoConnect(()=>{
+    app.listen(process.env.PORT||5000,()=>console.log('server running'))
+})
---
+++ utils/database.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const {MongoClient} = require('mongodb');
+
+let _db;
+const mongoConnect = callback =>{
+    MongoClient.connect(process.env.MONGO_URI)
+    .then(client=>{
+        console.log('connected to db');
+        _db = client.db();
+        callback()
+    })
+    .catch(err=>{
+        console.log(err)
+        throw err;
+    });
+}
+
+const getDb = ()=>{
+    if (_db){
+        return _db
+    }
+    throw "No database found"
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
